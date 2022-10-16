80d18459e561f65af51f9705001851b2d6252a7f
YimingZhao-art <78484762+YimingZhao-art@users.noreply.github.com>
2022-10-16 21:03:06 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import Index from './src_28_API'
+
+export default class App extends Component {
+    render() {
+        return (
+           <Index/>
+        )
+    }
+}
